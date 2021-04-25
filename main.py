2fbf21b4fbb4ddbedcfd3dbfebf3dd65dfe161d1
Mask <maskman4830@gmail.com>
2021-04-25 21:25:00 +0900

    test
    First python bot

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("goodmorning"):
+        if client.user != message.author:
+            m = "おはようございます" + message.author.name + "さん！"
+            await message.channel.send(m)
+
+
+client.run("000000000000")
